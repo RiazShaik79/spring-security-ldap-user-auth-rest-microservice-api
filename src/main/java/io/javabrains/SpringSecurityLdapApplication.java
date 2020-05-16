c12bf47bc82899dde0bfccf32f205da12041ffc3
riaz.shaik <riaz.shaik@CPX-Z9QOOTV9YHT.dir.svc.accenture.com>
2020-05-16 17:03:07 +0100

    update

--- src/main/java/io/javabrains/SpringSecurityLdapApplication.java
+++ src/main/java/io/javabrains/SpringSecurityLdapApplication.java
@@ -10,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EntityScan
+@EnableDiscoveryClient
 public class SpringSecurityLdapApplication {
 
 	
